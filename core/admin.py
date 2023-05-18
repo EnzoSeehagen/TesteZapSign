0ffb44836d9e890d9e2f9ddcdbff9ff1d373cdd3
Enzo Seehagen <enzovonseehagen@gmail.com>
2023-05-18 17:30:49 -0300

    Finalizando criação dos modelos e configuração de URL

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Company, Doc, User
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Doc)
+admin.site.register(User)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,63 @@
+from django.core.validators import MinLengthValidator
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+
+class Company(models.Model):
+    LANGUAGE_CHOICES = (
+        ('pt', 'Português'),
+        ('es', 'Espanhol'),
+        ('en', 'Inglês'),
+    )
+
+    nome = models.CharField(max_length=255)
+    data_criacao = models.DateTimeField()
+    data_atualizacao = models.DateTimeField(null=True, blank=True)
+    fuso_horario = models.CharField(max_length=50, default="-03:00")
+    linguagem = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='pt')
+    usuarios_convidados = models.ManyToManyField('User', related_name='companhias_convidadas', blank=True)
+    usuario_criador = models.ForeignKey('User', on_delete=models.CASCADE, related_name='companhias_criadas', blank=True)
+    documentos_associados = models.ManyToManyField('Doc', related_name='companhias_associadas', blank=True)
+
+
+
+class Doc(models.Model):
+    nome = models.CharField(max_length=255)
+    deletado = models.BooleanField(default=False)
+    data_criacao = models.DateTimeField(blank=False, null=False)
+    data_atualizacao = models.DateTimeField(null=True, blank=True)
+    data_limite_assinatura = models.DateTimeField(blank=True, null=True)
+    assinado = models.BooleanField(default=False)
+    company_associada = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='documentos_associados_doc', blank=True)
+    usuario_criador = models.ForeignKey('User', on_delete=models.CASCADE, related_name='documentos_criados', blank=True)
+
+    def save(self, *args, **kwargs):
+        self.data_atualizacao = timezone.now()
+        super().save(*args, **kwargs)
+
+    def assinar(self):
+        if not self.assinado:
+            self.assinado = True
+            self.save()
+
+
+
+
+class User(models.Model):
+    email = models.EmailField(max_length=255)
+    data_ultima_redefinicao_senha = models.DateTimeField(null=True, blank=True)
+    email_verificado = models.BooleanField(default=False)
+    senha = models.CharField(max_length=255, validators=[MinLengthValidator(6)])
+    data_criacao = models.DateTimeField(null=True, blank=False)
+    data_atualizacao = models.DateTimeField(null=True, blank=True)
+    companhias_associadas = models.ManyToManyField('Company', related_name='usuarios_associados', blank=True)
+    companhia_original = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='usuarios_originais', null=True, blank=True)
+    documentos_associados = models.ManyToManyField('Doc', related_name='usuarios_associados', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.data_criacao = timezone.now()
+        self.data_atualizacao = timezone.now()
+        super().save(*args, **kwargs)
+
